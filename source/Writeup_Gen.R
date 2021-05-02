302d2a1adf5d07472093e6784110a7351cbc4ed8
ovrhuman <71412394+ovrhuman@users.noreply.github.com>
2021-05-02 12:49:14 +0100

    rm files, makefile, cell onto plot

---
+++ source/Writeup_Gen.R
@@ -0,0 +1,17 @@
+# GENERATE WRITEUP FUNCTION
+
+render_writeup = function (markdown_file, writeup_name,
+                           title, author, date,
+                             results, phenotype2genes, working_directory){
+  if (!file.exists(paste0(working_directory,"data/hp.obo"))){
+    download.file("http://purl.obolibrary.org/obo/hp.obo",
+                  paste0(working_directory,"data/hp.obo"), mode = "wb")
+  }
+  rmarkdown::render(markdown_file, params = list(
+    title = title,
+    author = author,
+    date = date,
+    results_data =paste0(working_directory,results),
+    phenotype_to_genes_txt_file = paste0(working_directory,phenotype2genes)),
+  output_file = paste0(writeup_name ))#, ".pdf"))
+}
