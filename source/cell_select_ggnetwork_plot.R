c33757d8be7032bf82b90492e785810e762fddeb
ovrhuman <71412394+ovrhuman@users.noreply.github.com>
2021-08-29 13:45:34 +0100

    More ont lvl plots
    Added two new ontology level plots to the figures_demo markdown. Also added get_ont_level function to cell_select_ggnetwork_plot.R source script and added a useful_functions.R script for random functions

--- source/cell_select_ggnetwork_plot.R
+++ source/cell_select_ggnetwork_plot.R
@@ -314,6 +314,27 @@ ggnetwork_plot_full = function(phenotype_to_genes, all_results_merged, hpo, dise
 }
 
 
+#' Get absolute ontology level
+#'
+#' This gets the absolute ontology level of a term (without consideration for the
+#' particular subset of the data you are looking at, as in the find_parent function).
+#'
+#' @param hpo The HPO ontology data object
+#' @param term_id HPO term ID <string>
+#' @example get_ont_level(hpo,"HP:0000003")
+#' @return returns the ontology level <numeric>
+#' @export
+get_ont_level = function(hpo,term_id) {
+   children = unique(setdiff(unlist(hpo$children[term_id]), term_id))
+   if (length(children) == 0) {
+      return(0)
+   } else {
+      return(1 + get_ont_level(hpo,children)) #<- recursion..
+   }
+}
+
+
+
 # a function that allows you to choose between the ggnetwork interactive plot and the ontoplot (better for printing maybe)
 # ggnetwork_or_ontoplot_full = function(onto_or_ggnet, phenotype_to_genes, all_results_merged,
 #                                       hpo, disease_descriptions, cell_type,
