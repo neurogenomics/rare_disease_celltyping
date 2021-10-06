54f54dd01f4dada030c3594582ce617dab60079d
ovrhuman <71412394+ovrhuman@users.noreply.github.com>
2021-10-06 14:13:57 +0100

    Deleted rarediseaseewce package dir

--- rd_package/RareDiseaseEWCE/R/globals.R
+++
@@ -1,5 +0,0 @@
-# Global variables
-utils::globalVariables(c("Cell", "CellType", "Phenotype", "Tissue", "fold",
-                       "heirarchy", "hover", "label", "n_signif",
-                       "signif_asterics", "theme_blank", "x", "xend",
-                       "y", "yend"))
