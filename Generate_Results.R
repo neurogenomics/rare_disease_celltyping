9999ccaff9b3d370cd5f78c12db7cf9e4a160207
ovrhuman <71412394+ovrhuman@users.noreply.github.com>
2021-10-19 13:22:49 +0100

    generate results
    just changed a couple of input parameters for generate_results.r

--- Generate_Results.R
+++ Generate_Results.R
@@ -11,16 +11,16 @@ ctd <- readRDS("data/CTD_Descartes_withplot.rds")
 ## Run analysis ################################################################
 all_results <- MultiEWCE::gen_results(ctd,
                                       phenotype_to_genes,
-                                      list_names = unique(phenotype_to_genes$Phenotype)[1:5],
+                                      list_names = unique(phenotype_to_genes$Phenotype),
                                       background_genes = unique(phenotype_to_genes$Gene),
                                       list_name_column = "Phenotype",
                                       gene_column = "Gene",
                                       results_dir = "results_test_191021",
                                       overwrite_past_analysis = FALSE,
-                                      reps = 10,
+                                      reps = 100000,
                                       annotLevel = 1,
                                       genelistSpecies = "human",
-                                      cores = 1,
+                                      cores = 8,
                                       MergeResults = TRUE)
 
 saveRDS(all_results, paste0("HPO_EWCE_Results ",stringr::str_replace_all(Sys.time(),":",""),".rds"))
